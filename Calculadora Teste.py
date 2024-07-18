92c53cba9d3ba93aa54cf3b87de3410314b72f0a
Kayque Oliveira <kayquedeoliveiracarvalho@gmail.com>
2024-07-17 23:25:30 -0300

    Calculadora Teste.py

--- Calculadora Teste.py
+++ Calculadora Teste.py
@@ -1,31 +1,43 @@
 while True:
-    operação = input('Escolha a operação (+, /, * ou -): ')
-    if operação not in ('+', '/', 'x', '*', '-'):
-        print('Operação inválida. Tente novamente.')
-        continue
 
-    number1 = input('Digite o primeiro número: ')
-    number2 = input('Digite o segundo número: ' + number1 + ' ' + operação + ' ')
+        operação = input('Escolha a operação (+, /, * ou -): ')
+        if operação not in ('+', '/', 'x', '*', '-'):
+            print('Operação inválida. Tente novamente.')
+            continue
 
-    try:
-        number1 = float(number1)
-        number2 = float(number2)
-    except ValueError:
-        print("Por favor, digite números válidos.")
-        continue
+        number1 = input('Digite o primeiro número: ')
+        number2 = input('Digite o segundo número: ' + number1 + ' ' + operação + ' ')
 
-    resposta_soma = float(number1) + float(number2)
-    resposta_div = float(number1) / float(number2)
-    resposta_mult = float(number1) * float(number2)
-    resposta_sub = float(number1) - float(number2)
+        try:
+            number1 = float(number1)
+            number2 = float(number2)
+        except ValueError:
+            print("Por favor, digite números válidos.")
+            continue
 
-    if operação == ("+"):
-        print (resposta_soma)
-    elif operação == ("/"):
-        print (resposta_div)
-    elif operação == ("*") or str('x'):
-        print (resposta_mult)
-    elif operação == ("-"):
-        print (resposta_sub)
-    else:
-        print ("Operação inválida.")
+        resposta_soma = float(number1) + float(number2)
+        resposta_div = float(number1) / float(number2)
+        resposta_mult = float(number1) * float(number2)
+        resposta_sub = float(number1) - float(number2)
+
+        if operação == ("+"):
+            print (resposta_soma)
+        elif operação == ("/"):
+            print (resposta_div)
+        elif operação == ("*") or str('x'):
+            print (resposta_mult)
+        elif operação == ("-"):
+            print (resposta_sub)
+        else:
+            print ("Operação inválida.")
+
+        while True:
+            repetir = input("Deseja realizar outra operação? (s/n): ").lower()
+            if repetir not in ('s', 'n'):
+                print('Digite uma alternativa válida.')
+            else:
+                break
+
+        if repetir == 'n':
+            print("Encerrando o programa.")
+            break
